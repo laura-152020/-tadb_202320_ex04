d76c7f051b956cd81d647221ee84d1d6a453deea
laura-152020 <104606089+laura-152020@users.noreply.github.com>
2023-11-05 22:22:34 -0500

    conectar

--- conexion.py
+++ conexion.py
@@ -1,9 +1,12 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from pymongo import MongoClient
 
-# Reemplaza 'DATABASE_URI' con la URL de conexión a tu base de datos
-engine = create_engine('mysql://root:@Laura1502@@127.0.0.1/examen_3')
+# Reemplaza 'mongodb://localhost:27017' con la URI de tu servidor MongoDB
+mongo_uri = 'mongodb://localhost:27017'
+client = MongoClient(mongo_uri)
 
-# Crea una sesión
-Session = sessionmaker(bind=engine)
-session = Session()
+# Selecciona la base de datos que deseas utilizar en MongoDB
+# Reemplaza 'nombre_de_la_base_de_datos'
+db = client['nombre_de_tu_base_de_datos']
+
+# En MongoDB, no necesitas definir una sesión como en SQLAlchemy
+# Puedes interactuar directamente con la base de datos MongoDB usando la variable 'db'
